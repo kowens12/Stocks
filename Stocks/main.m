b95c291f97f697a779d3653e2603b2922f6c7b49
Katherine Owens <katherineowens@Katherines-MacBook-Pro.local>
2017-05-03 13:42:44 -0400

    Initial Commit

---
+++ Stocks/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Stocks
+//
+//  Created by Katherine Owens on 5/3/17.
+//  Copyright © 2017 Katherine Owens. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
